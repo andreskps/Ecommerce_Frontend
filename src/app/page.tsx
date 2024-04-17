fbc93ff3df1975b9d5e517ccca1f8b49503fb993
andreskps <pipesierra146@gmail.com>
2024-04-17 13:11:03 -0500

    Add shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Nav } from "@/components/nav";
+import Image from "next/image";
+import { FaShoppingCart } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <Nav/>
+  );
+}
