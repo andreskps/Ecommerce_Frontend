87d52bef6cf82509fd6a28d84b3c19292a96e9a1
andreskps <pipesierra146@gmail.com>
2024-06-10 12:42:43 -0500

    feat: Update npm dependency for @vercel/analytics to version 1.3.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/providers/AuthProvider";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -58,6 +59,7 @@ export default function RootLayout({
           {children}
         {/* <FacebookPixelEvents /> */}
           <Toaster />
+          <Analytics/>
         </body>
       </html>
     </AuthProvider>
